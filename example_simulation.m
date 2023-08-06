87bcc5ac6e0eba1f79121c7f1e2770400bbc267d
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-06 20:45:23 +0800

    Update example_simulation.m

--- example_simulation.m
+++ example_simulation.m
@@ -1,3 +1,6 @@
+%------------------------------------------------------------------%
+%--- The simulation results of path tracking for various robots ---%
+%------------------------------------------------------------------%
 close all;
 clear;
 set(gcf,'unit', 'centimeters','Position', [17, 2, 17, 21])
