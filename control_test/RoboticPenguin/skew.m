459adc4e32372c1403b85fc2d8843e780f5494d6
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 13:40:31 +0800

    Update skew.m

--- control_test/RoboticPenguin/skew.m
+++ control_test/RoboticPenguin/skew.m
@@ -1,5 +1,7 @@
+%-----------------------------%
+%--- Skew-symmetric matrix ---%
+%-----------------------------%
 function y  = skew(x)
-    % skew-symmetric matrix
     if isvector(x) && numel(x) == 3
         y = [0 -x(3) x(2); 
              x(3) 0 -x(1); 
