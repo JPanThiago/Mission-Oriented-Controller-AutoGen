004d1c4bac09190b8ccfe35a6035b6daf1269018
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 13:43:23 +0800

    Update CvtMatY.m

--- control_test/RoboticPenguin/CvtMatY.m
+++ control_test/RoboticPenguin/CvtMatY.m
@@ -2,7 +2,7 @@
 %--- The rotation matrix around the y-axis ---%
 %---------------------------------------------%
 function rw2t = CvtMatY(angle)
-    rw2t = [cos(angle), 0,  sin(angle); 
-                     0, 1,           0; 
-           -sin(angle), 0, cos(angle)];
+    rw2t = [ cos(angle), 0,  sin(angle); 
+                      0, 1,           0; 
+            -sin(angle), 0, cos(angle)];
 end
