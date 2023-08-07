b0c4f3448ff3d5fd989205c813ac4983eca5d1fb
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 13:19:56 +0800

    Update CvtMatY.m

--- control_test/RoboticPenguin/CvtMatY.m
+++ control_test/RoboticPenguin/CvtMatY.m
@@ -1,5 +1,7 @@
+%-------------------------------------------------------%
+%--- Calculate the rotation matrix around the y-axis ---%
+%-------------------------------------------------------%
 function rw2t = CvtMatY(angle)
-    % rotation matrix around the y-axis
     rw2t = [cos(angle), 0,  sin(angle); 
                      0, 1,           0; 
            -sin(angle), 0, cos(angle)];
