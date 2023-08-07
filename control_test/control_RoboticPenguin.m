53c437c94983ad2d2a2d6a37ada81c47e9c6d38c
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 12:09:33 +0800

    Update control_RoboticPenguin.m

--- control_test/control_RoboticPenguin.m
+++ control_test/control_RoboticPenguin.m
@@ -1,3 +1,6 @@
+%------------------------------------------------------------------------------------------------------------------------%
+%--- The control function of the penguin-inspired robot based on the three-state model after dimensionality reduction ---%
+%------------------------------------------------------------------------------------------------------------------------%
 function [sim, loss_avg] = control_RoboticPenguin(budget, underopt)
     if nargin < 1
         budget = 27;
