43505e86f188d94b6e2d9c4eeb9bc69b57bc096c
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 13:38:09 +0800

    Update calcTailHydroForces.m

--- control_test/RoboticPenguin/calcTailHydroForces.m
+++ control_test/RoboticPenguin/calcTailHydroForces.m
@@ -1,5 +1,7 @@
+%--------------------------------------------------------%
+%--- Tail hydrodynamics of the penguin-inspired robot ---%
+%--------------------------------------------------------%
 function F =  calcTailHydroForces(tud, tlr, U, W)
-    % calculating tail hydrodynamics of the penguin-inspired robot
     %{
     tud:  the angle of the tail turning up and down
     tlr:  the angle of the tail turning left and right
