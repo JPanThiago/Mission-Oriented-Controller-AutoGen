e34243942eca677c762141764baecaa4a8f13f9f
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 23:00:49 +0800

    Update dynamic.m

--- control_test/dynamic.m
+++ control_test/dynamic.m
@@ -1,6 +1,6 @@
-%----------------------------------------------%
-%--- The dynamics based on the Koopman model---%
-%----------------------------------------------%
+%----------------------------------------%
+%--- The Koopman model-based dynamics ---%
+%----------------------------------------%
 function X = dynamic(XLift, UExe, A, B, C)
     X1 = A * XLift + B * UExe;
     X = C * X1;
