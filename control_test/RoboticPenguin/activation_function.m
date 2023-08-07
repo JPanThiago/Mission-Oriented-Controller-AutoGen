792e1e4ac3450db7c39be8a00dcea028b1e3ab87
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 13:33:29 +0800

    Update activation_function.m

--- control_test/RoboticPenguin/activation_function.m
+++ control_test/RoboticPenguin/activation_function.m
@@ -1,5 +1,7 @@
+%--------------------------------------------------------------------%
+%--- The activation function for back-propagation neural networks ---%
+%--------------------------------------------------------------------%
 function final_outputs = activation_function(input)
-    % the activation function for back-propagation neural networks
     final_outputs = input;
     for i = 1 : length(input)
         final_outputs(i) = 1 / (1.0 + exp(-1.0 * input(i)));
