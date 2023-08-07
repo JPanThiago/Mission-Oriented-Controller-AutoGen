738f76d0ea864e5c73e632ac3d752074978f71f3
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 13:19:32 +0800

    Update CvtMatX.m

--- control_test/RoboticPenguin/CvtMatX.m
+++ control_test/RoboticPenguin/CvtMatX.m
@@ -1,5 +1,7 @@
+%-------------------------------------------------------%
+%--- Calculate the rotation matrix around the x-axis ---%
+%-------------------------------------------------------%
 function rw2t = CvtMatX(angle)
-    % rotation matrix around the x-axis
     rw2t = [1,          0,            0; 
             0, cos(angle),  -sin(angle); 
             0, sin(angle),  cos(angle)];
