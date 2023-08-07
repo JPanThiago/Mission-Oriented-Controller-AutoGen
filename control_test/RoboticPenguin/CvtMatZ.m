5a1411eb468ee4f242b1408f06dda20402c7a792
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 13:20:31 +0800

    Update CvtMatZ.m

--- control_test/RoboticPenguin/CvtMatZ.m
+++ control_test/RoboticPenguin/CvtMatZ.m
@@ -1,5 +1,7 @@
+%-------------------------------------------------------%
+%--- Calculate the rotation matrix around the z-axis ---%
+%-------------------------------------------------------%
 function rw2t = CvtMatZ(angle)
-    % rotation matrix around the z-axis
     rw2t = [cos(angle), -sin(angle),  0; 
             sin(angle),  cos(angle),  0; 
                      0,           0, 1];
