440764d8bc1e4cde5f0df5013e0ea8296b23a2b2
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-07 12:10:57 +0800

    Update control_SoftRobot.m

--- control_test/control_SoftRobot.m
+++ control_test/control_SoftRobot.m
@@ -1,3 +1,6 @@
+%----------------------------------------------%
+%--- The control function of the soft robot ---%
+%----------------------------------------------%
 function [sim, loss_avg, loss_avg_un, loss_avg_noise] = control_softrobot(budget, underopt, datanoise, noise, failurecase)
     if nargin < 1
         budget = 27;
