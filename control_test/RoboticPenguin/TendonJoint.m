b0ea571dfae29d62f9f92868bf4d39c797dd16c3
Jie Pan <49445587+JPanThiago@users.noreply.github.com>
2023-08-10 00:24:44 +0800

    Update TendonJoint.m

--- control_test/RoboticPenguin/TendonJoint.m
+++ control_test/RoboticPenguin/TendonJoint.m
@@ -1,6 +1,6 @@
-%--------------------------------------------------------------------------%
-%--- The tail rotation angle based on a back-propagation neural network ---%
-%--------------------------------------------------------------------------%
+%------------------------------------------------------------------------%
+%--- The tail rotation angle based on back-propagation neural network ---%
+%------------------------------------------------------------------------%
 function final_outputs = TendonJoint(angle, wih_cfd1, hidden_biascfd1, who_cfd1, final_cfd1)
     hidden_inputs = wih_cfd1 * angle;
     hidden_outputs = activation_function(hidden_inputs + hidden_biascfd1);
